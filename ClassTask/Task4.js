afedcb17d6354deb7d6c274132ed63c9fbd99a01
Harshita Singh <harshitasingh9412@gmail.com>
2026-01-27 22:47:04 +0530

    [Harshita] Add: nodejs classTask added

--- ClassTask/Task4.js
+++ ClassTask/Task4.js
@@ -16,3 +16,52 @@ systemInfo.js
 logger.js
 app.js
 */
+
+
+// for systemInfo.js
+
+const os = require('os');
+
+function getSystemInfo() {
+    return {
+        cpuCount: os.cpus().length,
+        freeMemory: (os.freemem() / 1024 / 1024).toFixed(2) + ' MB',
+        totalMemory: (os.totalmem() / 1024 / 1024).toFixed(2) + ' MB',
+        platform: os.platform(),
+        timestamp: new Date().toLocaleString()
+    };
+}
+
+module.exports = getSystemInfo;
+
+// for logger.js
+
+const fs = require('fs');
+const path = require('path');
+
+function logToFile(data) {
+    const logPath = path.join(__dirname, 'system-log.txt');
+    const logEntry = `${data.timestamp} | CPU: ${data.cpuCount} | Free Mem: ${data.freeMemory} | Total Mem: ${data.totalMemory} | Platform: ${data.platform}\n`;
+    
+    fs.appendFile(logPath, logEntry, (err) => {
+        if (err) {
+            console.log('Failed to write log');
+        } else {
+            console.log('System data logged');
+        }
+    });
+}
+
+module.exports = logToFile;
+
+// for app.js
+
+const getSystemInfo = require('./systemInfo');
+const logToFile = require('./logger');
+
+console.log('System monitor started...');
+
+setInterval(() => {
+    const sysData = getSystemInfo();
+    logToFile(sysData);
+}, 5000);
